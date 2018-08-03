a8e67be7b880fd604e771bd50a42a7d89395ce22
Brendan Hasz <winsto99@gmail.com>
2018-08-02 22:56:28 -0500

    Allow overwriting input file if output not specified

--- latex2mj.py
+++ latex2mj.py
@@ -22,7 +22,7 @@ import re
 # Command line arguments
 p = argparse.ArgumentParser(description='Convert LaTeX-styled equations to MathJax-styled equations in Markdown')
 p.add_argument("FileIn", help="Filename of the input file (with LaTeX-styled equations)")
-p.add_argument("FileOut", default=None, help="Filename of the output file to write (with MathJax-styled equations)")
+p.add_argument("FileOut", nargs='?', default=None, help="Filename of the output file to write (with MathJax-styled equations)")
 args = p.parse_args()
 
 # Load file
@@ -32,6 +32,10 @@ with open(args.FileIn, "r") as f:
 # Find inlines and replace $x$ with \\(x\\)
 ml = r'50' #max length (in chars) for inlines
 O = re.sub(r'((?<!\$)(\$)([^\$]{1,'+ml+'}?)(\$)(?!\$))', r'\\\\( \3 \\\\)', I)
+
+# Overwrite input file if output not specified
+if args.FileOut is None:
+    args.FileOut = args.FileIn
 
 # Write output file
 with open(args.FileOut, "w") as f:
